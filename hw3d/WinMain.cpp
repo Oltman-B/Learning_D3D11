60d9f29d653f07dcf52e530b518bd3db7794f7a0
Baruch Oltman <baruch.oltman@trojans.dsu.edu>
2019-03-28 19:58:27 -0500

    Add project files.

---
+++ hw3d/WinMain.cpp
@@ -0,0 +1,141 @@
+#include <Windows.h>
+#include <d3d11.h>
+#include <d3dcommon.h>
+#include <d3dcompiler.h>
+#include <stdio.h>
+#pragma comment(lib,"d3dcompiler.lib")
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, "My Window!");
+		}
+		break;
+	case WM_KEYUP:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, "Kaboom!");
+		}
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int CALLBACK WinMain(
+HINSTANCE hInstance,
+HINSTANCE hPrevInstance,
+LPSTR lpCmdLine,
+int nCmdShow)
+{
+	const char* pClassName = "myWindowClass";
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	// create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"Hardware Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	DXGI_RATIONAL refreshRate = { 0 };
+	refreshRate.Numerator = 60;
+	refreshRate.Denominator = 1;
+
+	DXGI_MODE_DESC modeDesc = { 0 };
+	modeDesc.Width = 0;
+	modeDesc.Height = 0;
+	modeDesc.RefreshRate = refreshRate;
+	modeDesc.Format = DXGI_FORMAT_R16G16B16A16_FLOAT;
+	modeDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	modeDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+	DXGI_SAMPLE_DESC sampleDesc = { 0 };
+	sampleDesc.Count = 1;
+	sampleDesc.Quality = 0;
+
+	DXGI_SWAP_CHAIN_DESC swapDesc = { 0 };
+	swapDesc.BufferDesc = modeDesc;
+	swapDesc.SampleDesc = sampleDesc;
+	swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapDesc.BufferCount = 2;
+	swapDesc.OutputWindow = hWnd;
+	swapDesc.Windowed = TRUE;
+	swapDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	swapDesc.Flags = 0;
+
+	// handle creation flags for device context
+	UINT creationFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+#if defined(_DEBUG)
+	// Enable debug layer in debug mode
+		creationFlags |= D3D10_CREATE_DEVICE_DEBUG;
+#endif
+	IDXGISwapChain *swapChain;
+	ID3D11Device *d3dDevice;
+	ID3D11DeviceContext *d3dDeviceContext;
+
+	const D3D_FEATURE_LEVEL featureLevels[] = {
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+
+	HRESULT result = D3D11CreateDeviceAndSwapChain(
+		NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, creationFlags,
+		featureLevels, 2, D3D11_SDK_VERSION, &swapDesc, &swapChain, &d3dDevice, NULL,
+		&d3dDeviceContext);
+
+	d3dDevice->AddRef();
+	d3dDeviceContext->AddRef();
+	swapChain->AddRef();
+
+	//Compile shaders
+
+	// message pump
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		d3dDevice->Release();
+		d3dDeviceContext->Release();
+		swapChain->Release();
+		return -1;		
+	}
+	else
+	{
+		d3dDevice->Release();
+		d3dDeviceContext->Release();
+		swapChain->Release();
+		return msg.wParam;
+	}	
+}
